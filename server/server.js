8b254bac70a1d68ed03523a1e5d19e84b6d7d1e8
Bhishman <bh352449@dal.ca>
2024-07-07 23:30:45 -0300

    fix: adding cors config

--- server/server.js
+++ server/server.js
@@ -4,10 +4,28 @@ const {exec} = require('child_process');
 const fs = require('fs');
 const archiver = require('archiver');
 const util = require('util');
+const cors = require('cors');
 
 const port = 9000;
 
 const execPromise = util.promisify(exec);
+
+/* CORS */
+const allowedOrigins = [
+    'https://www.yoursite.com'
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+/* TODO: app.use(cors(corsOptions));*/
+app.use(cors());
 
 app.use(express.json());
 
@@ -15,7 +33,7 @@ app.get('/health', (req, res) => {
     return res.status(200).send('Health Route is working!');
 });
 
-app.post('/generate-zip', async (req, res) => {
+app.post('/generate-zip', async (req, res, next) => {
     const {runtime, packages} = req.body;
     const envDir = `/tmp/env_${Date.now()}`;
     const zipFilePath = `${envDir}/packages.zip`;
@@ -39,9 +57,6 @@ app.post('/generate-zip', async (req, res) => {
         output.on('close', () => {
             /* Step 4: Download zip after successful compression */
             res.download(zipFilePath, 'packages.zip', (err) => {
-                if (err) {
-                    return res.status(500).send('Error downloading zip file');
-                }
 
                 /* Step 5: Clean up the environment */
                 fs.rmSync(envDir, {recursive: true, force: true});
@@ -49,12 +64,18 @@ app.post('/generate-zip', async (req, res) => {
         });
 
         archive.on('error', (err) => {
-            return res.status(500).send('Error generating zip file');
+            return next(err);
         });
 
     } catch (err) {
-        return res.status(500).send('Internal Server Error');
+        return next(err);
     }
+});
+
+/* Error handling middleware */
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
